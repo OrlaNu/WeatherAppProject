1b2dd89d69439154c6bb76583473576077846f87
OrlaNu <orla_nugent@hotmail.co.uk>
2021-05-17 15:53:59 +0100

    Removed console log from current location button JS

--- src/index.js
+++ src/index.js
@@ -126,7 +126,6 @@ search("London");
 function searchLocation(position){
   let apiKey = "a654db2f9bd0b0e600c5ab56e23dc457";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(showTempreture);
 }
 
